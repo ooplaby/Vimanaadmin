ed19b8c2cdd55c797362a751172bf0ee6877e4d8
ooplaby <ooplaby@gmail.com>
2025-05-23 13:15:26 +0530

    Update panel.js

--- js/panel.js
+++ js/panel.js
@@ -175,12 +175,14 @@ async function loadContactRequests() {
 // Helper function to escape HTML content to prevent XSS
 function escapeHTML(str) {
     if (typeof str !== 'string') return '';
-    return str.replace(/[&<>"']/g, function (match) {
-        return {
-            '&': '&',
-            '<': '<',
-            '>': '>',
-            '"': '"'
-        }[match];
+    return str.replace(/[&<>"']/g, function(match) {
+        const escapeMap = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#39;'
+        };
+        return escapeMap[match];
     });
 }
